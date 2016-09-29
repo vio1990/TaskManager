9c7a567155492619dac18eae190a177992e32875
vio1990 <vio1990@ukr.net>
2016-09-29 17:48:09 +0300

    Task entity is done..

--- src/main/java/com/ozerian/TaskManagerCLI.java
+++ src/main/java/com/ozerian/TaskManagerCLI.java
@@ -1,7 +1,35 @@
 package com.ozerian;
 
+
+import com.ozerian.util.InOutData;
+
+import java.io.IOException;
+
 public class TaskManagerCLI {
     public static void main(String[] args) {
 
+        try {
+            boolean isContinue = true;
+            System.out.println("Hi! Welcome to th TaskManager program!");
+            do {
+
+                int choice = InOutData.enteredChoice();
+
+                switch (choice) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        isContinue = false;
+                        break;
+                }
+            } while (isContinue == true);
+
+            System.out.println("Thank you! Good bye!");
+
+        } catch (IOException ex) {
+            System.out.println("Wrong input data!");
+        }
     }
 }
--- src/main/java/com/ozerian/entity/Task.java
+++ src/main/java/com/ozerian/entity/Task.java
@@ -1,4 +1,53 @@
 package com.ozerian.entity;
 
+import java.util.Date;
+
 public class Task {
+
+    private long id;
+    private String name;
+    private Date doneDate;
+    private TaskPriority priority;
+
+    public Task() {
+    }
+
+    public Task(long id, String name, Date doneDate, TaskPriority priority) {
+        this.id = id;
+        this.name = name;
+        this.doneDate = doneDate;
+        this.priority = priority;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDoneDate() {
+        return doneDate;
+    }
+
+    public void setDoneDate(Date doneDate) {
+        this.doneDate = doneDate;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
+    }
 }
