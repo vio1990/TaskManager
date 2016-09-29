1eac320bff5e6d8a016aecf798a56c434c274759
vio1990 <vio1990@ukr.net>
2016-09-29 20:08:49 +0300

    toString method for Task entity.

--- src/main/java/com/ozerian/entity/Task.java
+++ src/main/java/com/ozerian/entity/Task.java
@@ -50,4 +50,14 @@ public class Task {
     public void setPriority(String priority) {
         this.priority = priority;
     }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", doneDate=" + doneDate +
+                ", priority='" + priority + '\'' +
+                '}';
+    }
 }
