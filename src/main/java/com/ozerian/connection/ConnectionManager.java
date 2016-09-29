518de69ea9548c385d53d7b6e06bd3e7370ef41c
vio1990 <vio1990@ukr.net>
2016-09-29 17:47:47 +0300

    Connection manager is ready.

--- src/main/java/com/ozerian/connection/ConnectionManager.java
+++ src/main/java/com/ozerian/connection/ConnectionManager.java
@@ -1,4 +1,31 @@
 package com.ozerian.connection;
 
-public class ConnectionManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class ConnectionManager {
+    private static final String URL = "jdbc:mysql://localhost:3306/taskmanager";
+    private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "root";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER_NAME);
+            try {
+                connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+                System.out.println("Connection success!");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("SQL Exception " + e);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Driver is not found!");
+        }
+        return connection;
+    }
+
 }
